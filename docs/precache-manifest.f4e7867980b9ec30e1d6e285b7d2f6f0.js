b845e4ccea71542b9f4f96ef91a0a47fc99b482b
mfregoso <mfregoso@users.noreply.github.com>
2018-11-28 13:38:50 -0800

    docs test

---
+++ docs/precache-manifest.f4e7867980b9ec30e1d6e285b7d2f6f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "1941ebab0ea041ec5181",
+    "url": "./static/js/main.1941ebab.chunk.js"
+  },
+  {
+    "revision": "2e077fc50abda335d21f",
+    "url": "./static/js/1.2e077fc5.chunk.js"
+  },
+  {
+    "revision": "1941ebab0ea041ec5181",
+    "url": "./static/css/main.983af9db.chunk.css"
+  },
+  {
+    "revision": "2e077fc50abda335d21f",
+    "url": "./static/css/1.638542df.chunk.css"
+  },
+  {
+    "revision": "f70fc1e657e9512dff77400200f49f79",
+    "url": "./index.html"
+  }
+];
